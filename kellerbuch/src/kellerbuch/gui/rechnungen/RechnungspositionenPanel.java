c691c70eca815eb7b2c132830fffe31e9c683f36
michael <michaelbrunner88@gmail.com>
2015-11-11 03:09:46 +0100

    added sum for every bill position

--- kellerbuch/src/kellerbuch/gui/rechnungen/RechnungspositionenPanel.java
+++ kellerbuch/src/kellerbuch/gui/rechnungen/RechnungspositionenPanel.java
@@ -11,17 +11,24 @@ import kellerbuch.fachlogik.Rechnungspositionen;
 import kellerbuch.fachlogik.Weine;
 import kellerbuch.fachlogik.Winzerbetrieb;
 
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
 public class RechnungspositionenPanel extends JPanel {
 
 	/**
 	 * Create the panel.
 	 * 
 	 */
-	
+
 	Winzerbetrieb wb;
 	JSpinner spinner;
 	JComboBox<Weine> comboBox;
-	
+	private JTextField txtGesamtpreis;
+
+	/**
+	 * @wbp.parser.constructor
+	 */
 	public RechnungspositionenPanel(Winzerbetrieb wb) {
 		this.wb = wb;
 		initPanel();
@@ -32,13 +39,27 @@ public class RechnungspositionenPanel extends JPanel {
 		initPanel();
 		comboBox.setSelectedItem(repos.getWein());
 		spinner.setValue(new Integer(repos.getMenge()));
+		setGesamtpreis(comboBox, spinner);
 	}
-	
+
+	private void setGesamtpreis(JComboBox<Weine> comboBox, JSpinner spinner) {
+		Weine w = (Weine) comboBox.getSelectedItem();
+		int anzahl = new Integer(spinner.getValue().toString());
+		double gesamtpreis = w.getPreis() * anzahl;
+		txtGesamtpreis.setText(Double.toString(gesamtpreis));
+	}
+
 	private void initPanel() {
-		setLayout(new GridLayout(0, 2, 0, 0));	
+		setLayout(new GridLayout(0, 3, 0, 0));
 		comboBox = new JComboBox<Weine>(new Vector<Weine>(wb.getWeinliste()));
-		add(comboBox);	
+		add(comboBox);
 		spinner = new JSpinner();
-		add(spinner);	
+		add(spinner);
+
+		txtGesamtpreis = new JTextField();
+		txtGesamtpreis.setHorizontalAlignment(SwingConstants.CENTER);
+		txtGesamtpreis.setEditable(false);
+		add(txtGesamtpreis);
+		txtGesamtpreis.setColumns(10);
 	}
 }
