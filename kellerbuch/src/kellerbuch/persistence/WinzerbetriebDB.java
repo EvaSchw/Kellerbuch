7a37b053d813483dc4b46f424afc19566c861721
EvaSchw <e.schwanzelberger@hotmail.de>
2015-12-15 21:51:06 +0100

    nochmals

--- kellerbuch/src/kellerbuch/persistence/WinzerbetriebDB.java
+++ kellerbuch/src/kellerbuch/persistence/WinzerbetriebDB.java
@@ -2,6 +2,9 @@ package kellerbuch.persistence;
 
 import javax.persistence.*;
 
+import kellerbuch.fachlogik.Kunde;
+import kellerbuch.fachlogik.Rechnung;
+import kellerbuch.fachlogik.Weine;
 import kellerbuch.fachlogik.Winzerbetrieb;
 
 public class WinzerbetriebDB extends Winzerbetrieb
@@ -17,6 +20,82 @@ public class WinzerbetriebDB extends Winzerbetrieb
 		catch(Exception e)
 		{
 			throw new Exception("kann nicht zur Datenbank verbinden", e);
+		}
+	}
+	
+	@Override
+	public void weineAnlegen(Weine w) throws Exception
+	{
+		try
+		{
+			em.getTransaction().begin();
+			if(!em.contains(w))
+				em.persist(w);
+			else
+				throw new Exception("Dieser Wein ist bereits vorhanden!");
+			
+			em.getTransaction().commit();
+		}
+		catch (Exception e)
+		{
+			em.getTransaction().rollback();
+			throw new Exception("Fehler beim Speichern eines Weines in die Datenbank!", e);
+		}
+	}
+	
+	@Override
+	public void kundenAnlegen(Kunde k) throws Exception
+	{
+		try
+		{
+			em.getTransaction().begin();
+			if(!em.contains(k))
+				em.persist(k);
+			else
+				throw new Exception("Dieser Kunde ist bereits vorhanden!");
+			
+			em.getTransaction().commit();
+		}
+		catch (Exception e)
+		{
+			em.getTransaction().rollback();
+			throw new Exception("Fehler beim Speichern eines Kunden in die Datenbank!", e);
+		}
+	}
+	
+	@Override
+	public void rechnungAnlegen(Rechnung r) throws Exception
+	{
+		try
+		{
+			em.getTransaction().begin();
+			if(!em.contains(r))
+				em.persist(r);
+			else
+				throw new Exception("Diese Rechnung ist bereits vorhanden!");
+			
+			em.getTransaction().commit();
+		}
+		catch (Exception e)
+		{
+			em.getTransaction().rollback();
+			throw new Exception("Fehler beim Speichern einer Rechnung in die Datenbank!", e);
+		}
+	}
+	
+	@Override
+	public void weineloeschen(Weine w) throws Exception
+	{
+		try
+		{
+			em.getTransaction().begin();
+			em.remove(w);
+			em.getTransaction().commit();
+		}
+		catch (Exception e)
+		{
+			em.getTransaction().rollback();
+			throw new Exception("Fehler beim Löschen eines Weines", e);
 		}
 	}
 }
